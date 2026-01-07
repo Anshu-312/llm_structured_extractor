27b6fa1cfac4446a09c93d17ea174371188937f6
Anshu <anshu@insetweb.com>
2026-01-07 18:59:34 +0530

    Added repo structure and initial config

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+class settings(BaseSettings):
+    openrouter_api_key: str= Field(..., env="OPENROUTER_API_KEY")
+    openrouter_model: str= Field(..., env="OPENROUTER_MODEL")
+    debug: bool= Field(..., env="DEBUG")
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
